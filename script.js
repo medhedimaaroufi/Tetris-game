f2a990b6c3551c42a3f8c46c027f54eaec1b2312
medhedimaaroufi <149690463+medhedimaaroufi@users.noreply.github.com>
2023-12-17 00:58:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,83 @@
-window.addEventListener('load',function createGrid() {
-    var grid=document.querySelector('.grid');
-    for (let i = 0; i <200; i++) {
-        let child=document.createElement('div');
-        grid.appendChild(child);
-    }
-})
+window.addEventListener('DOMContentLoaded',() => {
+    const GRID_WIDTH = 10
+    const GRID_HEIGHT = 20
+    const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT
+    const colores=[
+        'URL(images/blue_block.jpg)',
+        'URL(images/purple_block.png)',
+        'URL(images/green_block.png)',
+        'URL(images/peach_block.png)',
+        'URL(images/yellow_block.png)',
+        'URL(images/pink_block.png)',
+        'URL(images/navy_block.png)'
+    ]
 
+    function createGrid() {
+        var grid=document.querySelector('.grid');
+        for (let i = 0; i < GRID_SIZE; i++) {
+            let child=document.createElement('div');
+            grid.appendChild(child);
+        }
+        var previous=document.querySelector('.previous-grid');
+        for (let i = 0; i < 16; i++) {
+            let element = document.createElement('div');
+            previous.appendChild(element);
+        }
+        return grid;
+    }
+
+    const grid = createGrid();
+    let squares = Array.from(grid.querySelectorAll('div'));
+    squares[0].classList.add('block');
+    
+    const lTetromino = [
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, 2],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2],
+        [GRID_WIDTH, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2]
+      ]
+    
+    const zTetromino = [
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+      ]
+    
+    const tTetromino = [
+        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+        [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+      ]
+    
+    const oTetromino = [
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+      ]
+    
+    const iTetromino = [
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
+      ]
+    
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+
+    for (let i = 0; i < 4; i++) {
+        let position=Tetromino[0][i]
+        squares[position].style.backgroundImage=colores[4];
+        squares[position].classList.add('block');
+    }
+
+    
+    
+    console.log(squares);
+
+
+
+})
