5b1fe77ed21c10623307e2f99a7dd1efbce6c4bf
medhedimaaroufi <149690463+medhedimaaroufi@users.noreply.github.com>
2023-12-19 18:13:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.addEventListener('DOMContentLoaded',() => {
+    $('.Game').hide();
+    $('.close').click(()=>{
+      $('.Rules').hide();
+      $('.Game').show();
+    });
+  
     const GRID_WIDTH = 10
     const GRID_HEIGHT = 20
     const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT
@@ -18,6 +24,11 @@ window.addEventListener('DOMContentLoaded',() => {
             let child=document.createElement('div');
             grid.appendChild(child);
         }
+        for (let i = 0; i < GRID_WIDTH; i++) {
+          let child=document.createElement('div');
+          child.classList.add('endGrid');
+          grid.appendChild(child);
+        }
         var previous=document.querySelector('.previous-grid');
         for (let i = 0; i < 16; i++) {
             let element = document.createElement('div');
@@ -28,7 +39,8 @@ window.addEventListener('DOMContentLoaded',() => {
 
     const grid = createGrid();
     let squares = Array.from(grid.querySelectorAll('div'));
-  
+
+    
     const lTetromino = [
         [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, 2],
         [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
@@ -64,222 +76,184 @@ window.addEventListener('DOMContentLoaded',() => {
         [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
       ]
     
+    
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
-
-
-
     let orient=0;
-    function draw(pos , piece){
-          for (let i = 0; i < 4; i++) {
-            let position=theTetrominoes[piece][orient][i] ;
-            squares[position + pos].style.backgroundImage=colores[piece];
-            squares[position + pos ].classList.add('block');
-          }
-        }
-    function remove(pos , piece){
+    
+    function draw(current, tetromino) {
       for (let i = 0; i < 4; i++) {
-        let position=theTetrominoes[piece][orient][i]
-        squares[position + pos].style.backgroundImage='none';
-        squares[position + pos ].classList.remove('block');
+        let position=theTetrominoes[tetromino][orient][i]
+        squares[current+position].style.backgroundImage=colores[tetromino];
+        squares[current+position].setAttribute('class','block');
+      }
+    }
+
+    function remove(current,tetromino) {
+      for (let i = 0; i < 4; i++) {
+        let position=theTetrominoes[tetromino][orient][i]
+        squares[current+position].style.backgroundImage = 'none' ;
+        squares[current+position].classList.remove('block');
+      }
+    }
+
+    let previous=$('.previous-grid div');
+    function drawNext(tetromino) {
+      for (let i = 0; i < 4; i++) {
+        let pos=theTetrominoes[tetromino][0][i]
+        pos=Math.floor(pos/GRID_WIDTH)*4+(pos%GRID_WIDTH);
+        previous[pos].style.backgroundImage=colores[tetromino];
+        previous[pos].setAttribute('class','block');
+      }
+    }
+
+    function removeNext(tetromino) {
+      for (let i = 0; i < 4; i++) {
+        let pos=theTetrominoes[tetromino][0][i]
+        pos=Math.floor(pos/GRID_WIDTH)*4+(pos%GRID_WIDTH);
+        previous[pos].style.backgroundImage = 'none' ;
+        previous[pos].classList.remove('block');
       }
     }
 
     function control(e) {
-      if (e.keyCode===37)   //ArrowLeft
-        left()
-      if (e.keyCode===38)   //ArrowUp
-        rotate()
-      if (e.keyCode===39)   //ArrowRight
-        right()
-      if (e.keyCode===40)   //ArrowDown 
-        gravity()
+      if (started && !paused){
+        if (e.keyCode===37)   //ArrowLeft
+          moveLeft()
+        if (e.keyCode===38)   //ArrowUp
+          Rotate()
+        if (e.keyCode===39)   //ArrowRight
+          moveRight()
+        if (e.keyCode===40)   //ArrowDown 
+          moveDown()
+        }
     }
 
     document.addEventListener('keydown', control)
-
-    let random=Math.floor(Math.random()*theTetrominoes.length)
-    let line = 0 ;
-    let col = 2 ; 
-    function gravity() {
-      remove(line*GRID_WIDTH+col,random);
-      if(!checkCollision('down')){
-          line++;}
-          draw(line*GRID_WIDTH+col,random) ;
-
+    
+    function generate() {
+      return Math.floor(Math.random()*theTetrominoes.length)
     }
-    function right() { 
+    
+    let random=generate()
+    let nextrandom=generate()
+    let started=false;
+    let paused=false;
+    drawNext(nextrandom)
+    let current=theTetrominoes[random]
+    let col=3;
+    let line=0;
+    draw(line*GRID_WIDTH+col,random);
+    $('.grid').hide();
+    $('.previous-grid').hide();
+    
+    function moveDown(){
       remove(line*GRID_WIDTH+col,random);
-      if(!checkCollision('right')){ col++ ; }
+      line++;
+      draw(line*GRID_WIDTH+col,random);
+      freeze();
+    }
+
+    function moveLeft() {
+      
+      let sumleft=0;
+      for(let i=0;i<4;i++){
+        let index=theTetrominoes[random][orient][i];
+        sumleft+=((line*GRID_WIDTH+col+index)%GRID_WIDTH>0);
+      }
+      let stopLeft=(sumleft==4);
+      if (stopLeft){
+        remove(line*GRID_WIDTH+col,random);
+        col--;
         draw(line*GRID_WIDTH+col,random);
-   }
-  
-   function left() { 
-    remove(line*GRID_WIDTH+col,random);
-    if(!checkCollision('left')){
-    col-- ;}
-    draw(line*GRID_WIDTH+col,random);
-   }
-   //function that checks if their is any collision after the rotation 
-   function checkCollisionAfterRotation() {
-    const nextOrientation = (orient + 1) % 4; // Calculate the next orientation after rotation
-  
-    for (let i = 0; i < 4; i++) {
-      const index = theTetrominoes[random][nextOrientation][i];
-      const nextPosition = line * GRID_WIDTH + col + index;
-  
-      // Check collision with walls
-      const isAtLeftEdge = nextPosition % GRID_WIDTH === 0;
-      const isAtRightEdge = (nextPosition + 1) % GRID_WIDTH === 0;
-  
-      // Check collision with other tet
-      const isOccupied = squares[nextPosition].classList.contains('block');
-  
-      if (isAtLeftEdge || isAtRightEdge || isOccupied) {
-        return true; // Collision detected
-      }
-    }
-  
-    return false; // No risque 
-  }
-  
-   function rotate(){
-    remove(line*GRID_WIDTH+col,random);
-    if(!checkCollisionAfterRotation()){
-    orient++;
-    orient%4;}
-    draw(line*GRID_WIDTH+col,random);
-   }
-   const displayWidth = 4
-   const displaySquares = document.querySelectorAll('.previous-grid div')
-   let displayIndex = 0
- 
-     const smallTetrominoes = [
-    [1, displayWidth + 1, displayWidth * 2 + 1, 2], /* lTetromino */
-    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], /* zTetromino */
-    [1, displayWidth, displayWidth + 1, displayWidth + 2], /* tTetromino */
-    [0, 1, displayWidth, displayWidth + 1], /* oTetromino */
-    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] /* iTetromino */
-  ] 
-
-  function displayNextTet(){
-    let nextrandom=Math.floor(Math.random()*theTetrominoes.length)
-    
-        // tnadhaf el tet la9dim
-      displaySquares.forEach(function(square) {
-      square.classList.remove('block');
-      square.style.backgroundImage = 'none';
-      });
-        // display el tet el jdid 
-      smallTetrominoes[nextrandom].forEach(function(index) {
-      const displaySquare = displaySquares[displayIndex + index];
-      displaySquare.classList.add('block');
-      displaySquare.style.backgroundImage = colores[nextrandom];
-      });
-    }
-
-    //function that checks the collision !
-    function checkCollision(direction) {
-      for (let i = 0; i < 4; i++) {
-        const index = theTetrominoes[random][orient][i];
-        const nextPosition = line * GRID_WIDTH + col + index;
-    
-        // Check collision with walls
-        const isAtLeftEdge = nextPosition % GRID_WIDTH === 0;
-        const isAtRightEdge = (nextPosition + 1) % GRID_WIDTH === 0;
-    
-        // Check collision with other tet
-        const isOccupied = squares[nextPosition].classList.contains('block');
-    
-        // Check collision with the bottom
-        const isAtBottom = nextPosition >= GRID_SIZE - GRID_WIDTH;
-    
-        switch (direction) {
-          case 'left':
-            if (isAtLeftEdge || isOccupied) {
-              return true; // Collision with left wall or other tet
-            }
-            break;
-          case 'right':
-            if (isAtRightEdge || isOccupied) {
-              return true; // Collision with right wall or other tet
-            }
-            break;
-          case 'down':
-            if (isAtBottom || isOccupied) {
-              return true; // Collision with the bottom or other tet below
-            }
-            break;
-          default:
-            break;
-        }
-      }
-    
-      return false; // No risque
-    }
-    
-    function freeze() {
-      for (let i = 0; i < 4; i++) {
-        const index = theTetrominoes[random][orient][i];
-        const position = line * GRID_WIDTH + col + index;
-        squares[position].style.backgroundImage = colores[random];
-        squares[position].classList.add('frozen'); 
-      }
-      checkRow();
-      //spawnNewTetromino();
-    }
-    
-    function unfreeze() {
-      for (let i = 0; i < 4; i++) {
-        const index = theTetrominoes[random][orient][i];
-        const position = line * GRID_WIDTH + col + index;
-        squares[position].classList.remove('frozen');
-      }
-    }
-    
-    function checkRow() {
-      for (let i = 0; i < GRID_SIZE; i += GRID_WIDTH) {
-        const row = Array.from(squares.slice(i, i + GRID_WIDTH));
-        if (row.every((square) => square.classList.contains('frozen'))) {
-          // Remove the row and move the above rows down
-          row.forEach((square) => {
-            square.style.backgroundImage = 'none';
-            square.classList.remove('frozen');
-          });
-          squares = [...squares.slice(0, i), ...squares.slice(i + GRID_WIDTH)];
-          squares.forEach((square) => grid.appendChild(square));
-        }
       }
     }
 
+    function moveRight() {
+      let sumright=0;
+      for(let i=0;i<4;i++){
+        let index=theTetrominoes[random][orient][i];
+        sumright+=((line*GRID_WIDTH+col+index)%GRID_WIDTH<9);
+      }
+      let stopRight=(sumright==4);
+      if (stopRight){
+        remove(line*GRID_WIDTH+col,random);
+        col++;
+        draw(line*GRID_WIDTH+col,random);
+      }
+    }
 
-    let isGameRunning = false ;
-    let x=1;
-    let Start=document.getElementById('startBtn');
-    let Pause=document.getElementById('pauseBtn');
-    Start.addEventListener('click',function () {
-      var timerId = setInterval(gravity, 1000);
+    function Rotate() {
+      remove(line*GRID_WIDTH+col,random);
+      orient++;
+      orient%=4;
+      draw(line*GRID_WIDTH+col,random);
+    }
+
+
+    //Start & Restart
+    const dashboard=$('.dashboard')
+    let level=1000;
+    let Start=document.querySelector('.start');
+    Start.addEventListener('click',repeated);
+
+    function repeated(){
+      $('.grid').show();
+      $('.previous-grid').show();
+      started=true;
+      var timerId = setInterval(moveDown, level);
       Start.setAttribute('value','Restart');
-      x++;
-      if (x>2) {
-        location.reload();
+      $('.dashboard .buttons').html('<input type="button" value="RESTART" class="restart button"><input type="button" value="PAUSE" class="pause button">')
+      
+      let x = 0;
+      $('.restart').click(function (){
+        if (++x >0) {
+          location.reload();
+        }
+      });
+
+      $('.pause').click(function () {
+        paused=true;
+        $('.grid').hide();
+        $('.previous-grid').hide();
+        clearInterval(timerId);
+        timerId=null;
+        $('.pause').val('RESUME');
+        $('.pause').removeClass('pause').addClass('resume');
+        $('.resume').click(function (){
+        paused=false;
+        $('.grid').show();
+        $('.previous-grid').show();
+        $('.resume').val('PAUSE');
+        $('.resume').removeClass('resume').addClass('pause');
+        repeated();
+      });
+      });
+
+      
+    }
+
+    function freeze(){
+      let stop=theTetrominoes[random][orient].some(index => (squares[(line+1)*GRID_WIDTH+col+index].className=='endGrid' || squares[(line+1)*GRID_WIDTH+col+index].className=='Freezed'));
+      if (stop){
+        theTetrominoes[random][orient].forEach(index => {
+          squares[line*GRID_WIDTH+col+index].setAttribute('class','Freezed');
+          console.log(squares[line*GRID_WIDTH+col+index]);
+        });
+        line=0
+        col=4
+        orient=0
+        random=nextrandom
+        removeNext(nextrandom)
+        nextrandom=generate()
+        drawNext(nextrandom)
       }
-    Pause.addEventListener('click',function(){
-      if(Pause.value === 'Pause'){ 
-        Pause.setAttribute('value','Resume') ; 
-        freeze() ; }
-      else if (Pause.value === 'Resume'){
-        Pause.setAttribute('value','Pause') ;
-        unfreeze() ;
-      }
-     
-
-    })
-      displayNextTet() ;
-    });
+    }
+    
+    console.log(previous)
+    
 
 
-    console.log(squares);
-
-
-
-      })
+    //TO DO: 19-12-2023
+    //Game Over
+    //check Row
+})
